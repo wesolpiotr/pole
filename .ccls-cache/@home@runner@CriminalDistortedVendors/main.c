ffc05e5ebdfbe2953e2c496992989479716ca5af
wesolpiotr <100830679+wesolpiotr@users.noreply.github.com>
2022-03-03 08:25:29 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@CriminalDistortedVendors/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define pi 3.14
+
+int main(void) {
+  int r = 10;
+  double pole = pi*r*r;
+  printf("Pole jest równe: %f", pole);
+  return 0;
+}
+
+/* 
+1. Dowiedzieć się na czym polega funkcja scan (???) i przetestować jej użycie (wprowadzanie danych do programu)
+2. Zobić przynajmniej 3 pierwsze zadania
+*/
---
+++ .ccls-cache/@home@runner@CriminalDistortedVendors/poletrapezu.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = 10;
+  int b = 5;
+  int h = 2;
+  int pole = ((a+b)*h)/2;
+  printf("Pole jest równe: %f", pole);
+  return 0;
+}
